d4d4c2f4bf6f042506e5527f103c1019f5f29b89
Vicky Nanda Ferdianto <132178127+vickynfrdnto@users.noreply.github.com>
2024-10-29 18:51:22 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 var typed= new Typed(".text", {
-    strings: ["Jr FrontEnd Web Developer" , "Jr Graphic Designer" , "Jr System Analyst" , "Jr System Support" , "Database" , "Technical Support"],
+    strings: ["Data Analyst" , "FrontEnd Web Developer" , "Graphic Designer" , "System Analyst" , "Technical Support"],
     typeSpeed:100,
     backSpeed:100,
     backDelay:1000,
